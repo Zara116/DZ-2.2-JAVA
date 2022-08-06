adace39481b70d24d9d22fa417c50a3fe41b909b
Zara116 <zarema.mindubaeva@yandex.ru>
2022-08-06 13:19:48 +0300

    reformated code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int payment=1100;
+        int payment = 1100;
         int bonus;
         int newbalance;
 
 
         if (payment > 1000) {
-           bonus = payment/100; newbalance=balance+payment+bonus;
-        } else  {
-            newbalance=balance+payment;
+            bonus = payment / 100;
+            newbalance = balance + payment + bonus;
+        } else {
+            newbalance = balance + payment;
 
         }
-        System.out.println("Итого баланс:"+newbalance);
+        System.out.println("Итого баланс:" + newbalance);
     }
 }
